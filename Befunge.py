cf4a02c66f6912f0f607d44c3cc53b27f566ae87
Nitin Aggarwal <nitin.aggrwal@bcause.in>
2016-10-18 17:11:00 +0530

    updates

--- Befunge.py
+++ Befunge.py
@@ -2,29 +2,29 @@ def interpret(code):
 	
 	arr = [[y for y in x] for x in code.split('\n')]
 	print(arr)
+	stack = []
 	output = ""
-	stack = ""
 	
 	from numpy import random
 	
 	bDict = {
-		'0' : lambda output: '0' + output,
-		'1' : lambda output: '1' + output,
-		'2' : lambda output: '2' + output,
-		'3' : lambda output: '3' + output,
-		'4' : lambda output: '4' + output,
-		'5' : lambda output: '5' + output,
-		'6' : lambda output: '6' + output,
-		'7' : lambda output: '7' + output,
-		'8' : lambda output: '8' + output,
-		'9' : lambda output: '9' + output,
-		'+' : lambda output: str(int(output[0]) + int(output[1])) + output[2:],
-		'-' : lambda output: str(int(output[0]) - int(output[1])) + output[2:],
-		'*' : lambda output: str(int(output[0]) * int(output[1])) + output[2:],
-		'/' : lambda output: str(int(output[0]) / int(output[1])) + output[2:],
-		'%' : lambda output: str(int(output[0]) % int(output[1])) + output[2:],
-		'!' : lambda output: '1' + output[1:] if output[0] == '0' else '0',
-		"'" : lambda output: '1' + output[2:] if output[0] < output[1] else '0',
+		'0' : lambda stack: [0] + stack,
+		'1' : lambda stack: [1] + stack,
+		'2' : lambda stack: [2] + stack,
+		'3' : lambda stack: [3] + stack,
+		'4' : lambda stack: [4] + stack,
+		'5' : lambda stack: [5] + stack,
+		'6' : lambda stack: [6] + stack,
+		'7' : lambda stack: [7] + stack,
+		'8' : lambda stack: [8] + stack,
+		'9' : lambda stack: [9] + stack,
+		'+' : lambda stack: [int(stack[0]) + int(stack[1])] + stack[2:],
+		'-' : lambda stack: [int(stack[0]) - int(stack[1])] + stack[2:],
+		'*' : lambda stack: [int(stack[0]) * int(stack[1])] + stack[2:],
+		'/' : lambda stack: [int(stack[0]) / int(stack[1])] + stack[2:],
+		'%' : lambda stack: [int(stack[0]) % int(stack[1])] + stack[2:],
+		'!' : lambda stack: [1] + stack[1:] if stack[0] == '0' else '0',
+		"'" : lambda stack: [1] + stack[2:] if stack[0] < stack[1] else '0',
 		
 		'>' : lambda x, y: [x, y + 1], 
 		'<' : lambda x, y: [x, y - 1],
@@ -33,26 +33,26 @@ def interpret(code):
 		
 		'?' : random.choice(['>','<','v','^']),
 		
-		'_' : lambda output: [output[1:], '>'] if output[1] == 0 else [output[1:], '<'], 
-		'|' : lambda output: [output[1:], '^'] if output[1] == 0 else [output[1:], 'v'],
+		'_' : lambda stack: [stack[1:], '>'] if stack[0] == '0' else [stack[1:], '<'], 
+		'|' : lambda stack: [stack[1:], '^'] if stack[0] == '0' else [stack[1:], 'v'],
 		
-		'"' : lambda output, a: [str(ord(a)) + output, 0] if a != '"' else [output, 1],
+		'"' : lambda stack, a: [[ord(a)] + stack, 0] if a != '"' else [stack, 1],
 		
-		':' : lambda output: output[0] + output if len(output) > 0 else "0",
-		'\\' : lambda output: output[1] + output[0] + output[2:] if len(output) > 1 else output +'0',
+		':' : lambda stack: [stack[0]] + stack if len(stack) > 0 else "0",
+		'\\' : lambda stack: [stack[1]] + [stack[0]] + stack[2:] if len(stack) > 1 else stack +'0',
 		
-		'$' : lambda output: output[1:],
+		'$' : lambda stack: stack[1:],
 		
-		'.' : lambda output: [output[1:], stack + str(int(output[0]))],
-		',' : lambda output: [output[1:], stack +  chr(int(output[0]))],
+		'.' : lambda stack: [stack[1:], int(stack[0])],
+		',' : lambda stack: [stack[1:], chr(stack[0])],
 		
 		'#' : lambda x, y, t: t(x, y),
 		
 		'p' : lambda p, q, v, arr: ord(v),
-		'g' : lambda p, q, output: str(ord(arr[p][q])) + output,
+		'g' : lambda p, q, stack: [ord(arr[p][q])] + stack,
 		
-		'@' : lambda output, x, y: [output, x, y, 1],
-		' ' : lambda output, x, y: [output, x, y, 1]
+		'@' : lambda stack, x, y: [stack, x, y, 1],
+		' ' : lambda stack, x, y: [stack, x, y, 1]
 	}
 	
 	t = 0
@@ -68,21 +68,21 @@ def interpret(code):
 		elif current == '?':
 			move = bDict[bDict['?']]
 		elif current in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '%', '!', "'", ":", "\\", '$']:
-			output = bDict[current](output)
+			stack = bDict[current](stack)
 		elif current in ['_', '|']:
-			print(x, y)
-			[output, dir] = bDict[current](output)
+			[stack, dir] = bDict[current](stack)
 			move = bDict[dir]
-			print(x, y)
+			
 		elif current == '"':
 			stop = 0
 			while (not stop):
-				[output, stop] = bDict['"'](output, current)
+				[stack, stop] = bDict['"'](stack, current)
 				x, y = move(x, y)
 				current = arr[x][y]
+				print(stack)
 		elif current in ['.', ',']:
-			[output, disp] = bDict[current](output)
-			stack = stack + str(disp)
+			[stack, disp] = bDict[current](stack)
+			output = output + str(disp)
 		elif current == '#':
 			x, y = bDict[current](x, y, move)
 		elif current == 'p':
@@ -93,7 +93,7 @@ def interpret(code):
 		elif current == 'g':
 			a = bDict['.'][1]
 			b = bDict['.'][1]
-			output = int(ord(arr[b][a])) + output
+			stack = int(ord(arr[b][a])) + stack
 		elif current == ' ':
 			pass
 		else:
@@ -101,7 +101,11 @@ def interpret(code):
 			
 		x, y = move(x, y)
 		current = arr[x][y]
-		print("x = %r, y = %r, current = %r. \n output = %r" % (x, y, current, output))
 		
-		print("output is actually %r" % stack)	
+		print("x = %r, y = %r, current = %r. \n stack = %r" % (x, y, current, stack))
+		
+		print("Output is actually %r" % output)	
+		
 	return output
+
+interpret('>987v>.v\nv456<  :\n>321 ^ _@')
